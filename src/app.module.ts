029a2f368cd86095b77723114a2e9a8fab92f583
鍾岳儒 <andy6756@eri.com.tw>
2022-12-02 18:00:10 +0800

    user Serve Static

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,15 @@ import { AppService } from './app.service';
 import { DocumentController } from './document/document.controller';
 import { CatsController } from './cats/cats.controller';
 import { DocumentService } from './document/document.service';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
-  imports: [],
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'client'),
+    }),
+  ],
   controllers: [AppController, DocumentController, CatsController],
   providers: [AppService, DocumentService],
 })
