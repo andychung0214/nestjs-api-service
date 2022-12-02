782fa47f3ffe7bb78493a0ee13ea2aef678b16c7
鍾岳儒 <andy6756@eri.com.tw>
2022-12-02 17:51:59 +0800

    add document service

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DocumentController } from './document/document.controller';
 import { CatsController } from './cats/cats.controller';
+import { DocumentService } from './document/document.service';
 
 @Module({
   imports: [],
   controllers: [AppController, DocumentController, CatsController],
-  providers: [AppService],
+  providers: [AppService, DocumentService],
 })
 export class AppModule {}
