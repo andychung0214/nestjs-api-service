1227a7c8de7a1b664c1fb32f23084a587631398e
鍾岳儒 <andy6756@eri.com.tw>
2022-12-02 17:16:52 +0800

    add route

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DocumentController } from './document/document.controller';
+import { CatsController } from './cats/cats.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [AppController, DocumentController, CatsController],
   providers: [AppService],
 })
 export class AppModule {}
